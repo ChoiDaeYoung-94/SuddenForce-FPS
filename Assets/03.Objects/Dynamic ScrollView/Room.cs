2376a124017333b67b1b7401169510ea43dc9451
ChoiDaeYoung-94 <chleodud1410@gmail.com>
2024-12-30 15:11:07 +0900

    RoomManage.cs 개선 및 refresh 기능 추가 #15

--- Assets/03.Objects/Dynamic ScrollView/Room.cs
+++ Assets/03.Objects/Dynamic ScrollView/Room.cs
@@ -11,8 +11,7 @@ namespace AD
     public class Room : MonoBehaviour
     {
         [Header("--- 세팅 ---")]
-        [SerializeField, Tooltip("Content에 RectTransform을 넘겨주기 위함")]
-        internal RectTransform _RTR_this = null;
+        [SerializeField] public RectTransform _RTR_this = null;
         [SerializeField] TMP_Text _TMP_roomName = null;
         [SerializeField] TMP_Text _TMP_gameSceneName = null;
         [SerializeField] TMP_Text _TMP_roomPlayerCount = null;
@@ -31,11 +30,6 @@ namespace AD
             _TMP_roomName.text = _sessionInfo.Name;
             _TMP_gameSceneName.text = _sessionInfo.Properties["Map"];
             _TMP_roomPlayerCount.text = $"{_sessionInfo.PlayerCount} / {_sessionInfo.MaxPlayers}";
-        }
-
-        internal int GetIndex()
-        {
-            return _sessionIndex;
         }
     }
 }
