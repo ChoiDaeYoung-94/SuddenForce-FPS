79d000fc7faca3961b9905213d4816463776a754
ChoiDaeYoung-94 <chleodud1410@gmail.com>
2025-04-09 22:42:17 +0900

    프로젝트 정리 #28

---
+++ Assets/Scripts/Managers/NetworkRunnerManager.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Fusion;
+using Fusion.Sockets;
+
+public class NetworkRunnerManager : MonoBehaviour, INetworkRunnerCallbacks
+{
+    private static NetworkRunnerManager _instance;
+    public static NetworkRunnerManager Instance { get { return _instance; } }
+
+    [Header("--- 세팅 ---")]
+    [SerializeField] private NetworkRunner _networkRunner = null;
+    [SerializeField] private NetworkSceneManagerDefault _networkSceneM = null;
+
+    private void Awake()
+    {
+        _instance = this;
+
+        Init();
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+
+    #region Functions
+
+    private void Init()
+    {
+        AD.Managers.PopupM.PopupLoading();
+        _networkRunner.AddCallbacks(this);
+
+        JoinSessionLobby();
+    }
+
+    #region Photon Fusion
+
+    public async void JoinSessionLobby()
+    {
+        _networkRunner.ProvideInput = false;
+
+        var result = await _networkRunner.JoinSessionLobby(SessionLobby.ClientServer);
+
+        AD.Managers.PopupM.ClosePopupLoading();
+
+        if (result.Ok)
+        {
+            AD.DebugLogger.Log("NetworkRunnerM", "JoinSessionLobby successfully.");
+        }
+        else
+        {
+            AD.DebugLogger.LogError("NetworkRunnerM", $"Failed to JoinSessionLobby: {result.ShutdownReason}");
+        }
+    }
+
+    public async void CreateRoom(object value)
+    {
+        AD.Managers.PopupM.PopupLoading();
+
+        Dictionary<string, object> temp_value = value as Dictionary<string, object>;
+
+        Dictionary<string, SessionProperty> sessionProperties = new Dictionary<string, SessionProperty>()
+        {
+            { "MapName", temp_value["MapName"].ToString() }
+        };
+
+        var startGameResult = await _networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Host,
+            SessionName = temp_value["RoomName"].ToString(),
+            PlayerCount = int.Parse(temp_value["MaxPlayers"].ToString()),
+            SessionProperties = sessionProperties,
+            IsVisible = bool.Parse(temp_value["IsPrivateRoom"].ToString()),
+            Scene = SceneRef.FromIndex(3),
+            SceneManager = _networkSceneM
+        });
+
+        AD.Managers.PopupM.ClosePopupLoading();
+
+        if (startGameResult.Ok)
+        {
+            AD.DebugLogger.Log("NetworkRunnerM", $"세션 생성 성공: {temp_value["RoomName"]}");
+        }
+        else
+        {
+            AD.DebugLogger.LogError("NetworkRunnerM", $"세션 생성 실패: {startGameResult.ShutdownReason}");
+        }
+    }
+
+    public async void JoinRoom(SessionInfo sessionInfo)
+    {
+        AD.Managers.PopupM.PopupLoading();
+
+        AD.DebugLogger.Log("NetworkRunnerM", $"Attempting to join session: {sessionInfo.Name}");
+
+        var joinResult = await _networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Client,
+            SessionName = sessionInfo.Name,
+            Scene = SceneRef.FromIndex(2),
+            SceneManager = _networkSceneM
+        });
+
+        AD.Managers.PopupM.ClosePopupLoading();
+
+        if (joinResult.Ok)
+        {
+            AD.DebugLogger.Log("NetworkRunnerM", $"Joined session successfully: {sessionInfo.Name}");
+        }
+        else
+        {
+            AD.DebugLogger.LogError("NetworkRunnerM", "Failed to join session: " + joinResult.ShutdownReason);
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Shutdown()
+    {
+        AD.Managers.PopupM.PopupLoading();
+
+        _networkRunner.Shutdown();
+    }
+
+    #endregion
+
+    #region INetworkRunnerCallbacks
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        AD.DebugLogger.Log("NetworkRunnerM", $"OnShutdown - {shutdownReason}");
+
+        AD.Managers.PopupM.ClosePopupLoading();
+
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == AD.GameConstants.Scene.Room.ToString())
+            AD.Managers.SceneM.ChangeScene(AD.GameConstants.Scene.Lobby);
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        AD.DebugLogger.Log("NetworkRunnerM", $"Session list updated. Count: {sessionList.Count}");
+
+        RoomManage.Instance.Init(sessionList);
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+
+    #endregion
+
+    #endregion
+}
---
+++ Assets/Scripts/Managers/PopupManager.cs
@@ -0,0 +1,221 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using TMPro;
+
+namespace AD
+{
+    /// <summary>
+    /// 팝업 관리 클래스
+    /// 게임 씬과 로비 씬에서 뒤로 가기 버튼 클릭 시 적절한 팝업을 표시
+    /// </summary>
+    public class PopupManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _popupCommon;
+        [SerializeField] TMP_Text _messageText;
+        [SerializeField] private GameObject _popupExit;
+        [SerializeField] private GameObject _popupGameOver;
+        [SerializeField] private GameObject _popupLoading;
+        [SerializeField] private GameObject _popupLobby;
+        [SerializeField] private GameObject _popupSetting;
+        [SerializeField] private GameObject _popupSceneLoading;
+        [SerializeField] private GameObject _bgmToggle;
+        [SerializeField] private GameObject _sfxToggle;
+
+        private Stack<GameObject> _popupStack = new Stack<GameObject>();
+
+        /// <summary>
+        /// 예외처리에 사용
+        /// 로비씬 -> 오퍼월
+        /// 게임씬에서 아이템을 선택하여서 사용 전인지에 대한 여부 판단
+        /// Flow상 MainScene 진입 시 CanvasLobby.cs에서 false
+        /// </summary>
+        private bool _isException = true;
+        private bool _isFlow = false;
+
+        /// <summary>
+        /// Managers - Awake() -> Init()
+        /// </summary>
+        public void Init()
+        {
+            AD.Managers.UpdateM.OnUpdateEvent -= OnUpdate;
+            AD.Managers.UpdateM.OnUpdateEvent += OnUpdate;
+
+            SetPopup();
+        }
+
+        /// <summary>
+        /// 열려 있는 모든 팝업을 비활성화하고 팝업 스택을 초기화
+        /// </summary>
+        public void SetPopup()
+        {
+            foreach (GameObject popup in _popupStack)
+            {
+                popup.SetActive(false);
+            }
+            _popupStack.Clear();
+        }
+
+        private void OnUpdate()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape) && !_isFlow)
+                {
+                    DisablePop();
+                }
+            }
+        }
+
+        #region Functions
+        /// <summary>
+        /// 팝업이 활성화될 때 스택에 추가
+        /// </summary>
+        public void EnablePop(GameObject popup)
+        {
+            _popupStack.Push(popup);
+            AD.DebugLogger.Log("PopupManager", $"_popupStack.Count: {_popupStack.Count}, 팝업 스택에 푸시됨");
+        }
+
+        /// <summary>
+        /// 팝업이 비활성화될 때 스택에서 제거
+        /// 팝업이 없으면 씬 상태에 따라 종료 팝업 또는 로비 전환 팝업을 표시
+        /// </summary>
+        public void DisablePop()
+        {
+            AD.Managers.SoundM.UI_Click();
+
+            if (_isException)
+            {
+                AD.DebugLogger.Log("PopupManager", $"{_isException} - 예외 처리 활성");
+                return;
+            }
+
+            if (_popupStack.Count > 0)
+            {
+                GameObject popup = _popupStack.Pop();
+                AD.DebugLogger.Log("PopupManager", $"_popupStack.Count: {_popupStack.Count} 팝업 스택에서 팝업 제거됨");
+                popup.SetActive(false);
+            }
+            else
+            {
+                //if (!AD.Managers.GameM.IsGame)
+                //{
+                //    AD.DebugLogger.Log("PopupManager", "lobby scene -> quit popup");
+
+                //    if (!_popupExit.activeSelf)
+                //        PopupExit();
+                //}
+                //else
+                //{
+                //    AD.DebugLogger.Log("PopupManager", "game scene-> go lobby popup");
+
+                //    if (!_popupLobby.activeSelf)
+                //        PopupGoLobby();
+                //}
+            }
+        }
+
+        internal void PopupLoading() => _popupLoading.SetActive(true);
+        internal void ClosePopupLoading() => _popupLoading.SetActive(false);
+
+        public void PopupSceneLoading() => _popupSceneLoading.SetActive(true);
+        public void ClosePopupSceneLoading() => _popupSceneLoading.SetActive(false);
+
+        internal void PopupMessage(string message)
+        {
+            _messageText.text = message;
+            _popupCommon.SetActive(true);
+        }
+
+        public void PopupGoLobby()
+        {
+            UnityEngine.Time.timeScale = 0;
+            _popupLobby.SetActive(true);
+        }
+
+        public void PopupExit()
+        {
+            UnityEngine.Time.timeScale = 0;
+            _popupExit.SetActive(true);
+        }
+
+        public void PopupGameOver() => _popupGameOver.SetActive(true);
+
+        public void PopupSetting()
+        {
+            UnityEngine.Time.timeScale = 0;
+
+            float bgm = PlayerPrefs.GetFloat("BGM", 1f);
+            float sfx = PlayerPrefs.GetFloat("SFX", 1f);
+
+            // 토글 오브젝트 활성화 여부: 기본 값 1이면 비활성화, 그 외 활성화
+            _bgmToggle.SetActive(bgm != 1f);
+            _sfxToggle.SetActive(sfx != 1f);
+
+            _popupSetting.SetActive(true);
+        }
+
+        public void ClosePopupSetting() => UnityEngine.Time.timeScale = 1;
+
+        public void GoLobby()
+        {
+            UnityEngine.Time.timeScale = 1;
+
+            //AD.Managers.GameM.SwitchMainOrGameScene(AD.GameConstants.Scenes.Main);
+        }
+
+        //public void GameOver() => AD.Managers.GameM.GameOverGoLobby();
+
+        public void ExitGame() => Application.Quit();
+
+
+        public void SetException() => _isException = true;
+
+        public void ReleaseException() => _isException = false;
+
+        public void SetFlow() => _isFlow = true;
+
+        public void ReleaseFlow() => _isFlow = false;
+
+        public void BGM()
+        {
+            float bgm = PlayerPrefs.GetFloat("BGM", 1f);
+
+            if (bgm == 1f)
+            {
+                _bgmToggle.SetActive(true);
+                bgm = 0f;
+            }
+            else
+            {
+                _bgmToggle.SetActive(false);
+                bgm = 1f;
+            }
+
+            AD.Managers.SoundM.SetBGMVolume(bgm);
+            PlayerPrefs.SetFloat("BGM", bgm);
+        }
+
+        public void SFX()
+        {
+            float sfx = PlayerPrefs.GetFloat("SFX", 1f);
+
+            if (sfx == 1f)
+            {
+                _sfxToggle.SetActive(true);
+                sfx = 0f;
+            }
+            else
+            {
+                _sfxToggle.SetActive(false);
+                sfx = 1f;
+            }
+
+            AD.Managers.SoundM.SetSFXVolume(sfx);
+            PlayerPrefs.SetFloat("SFX", sfx);
+        }
+        #endregion
+    }
+}
